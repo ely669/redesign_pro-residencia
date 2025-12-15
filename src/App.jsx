3f5443e505ffe38e69816b4dc0edf6cf9586f711
ely669 <72584111+ely669@users.noreply.github.com>
2025-12-15 12:18:33 -0300

    Inicio

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+// Importando o componente Dashboard
+import Dashboard from '../dashboard.jsx'
+
+function App() {
+  return <Dashboard />
+}
+
+export default App
+
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./dashboard.jsx",
+    "./*.{js,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
